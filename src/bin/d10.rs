4a3705053a21b13fb2ca43568b5f3a963fa310ac
s33b00r <qubert00@live.se>
2024-12-10 07:13:59 +0100

    Update Day 10

--- src/bin/d10.rs
+++ src/bin/d10.rs
@@ -3,26 +3,24 @@ use y24::{args, result};
 
 const BIN: &str = env!("CARGO_BIN_NAME");
 
-fn find_trails(map: &Vec<Vec<u32>>, x: usize, y: usize, cur: u32, seen_ends: &mut HashSet<(usize, usize)>) -> u32 {
-    if cur == 9 && !seen_ends.contains(&(x, y)) { 
-        seen_ends.insert((x, y));
-        return 1;
+fn find_trails_or_ratings(map: &Vec<Vec<u32>>, x: usize, y: usize, cur: u32, seen_ends: &mut Option<HashSet<(usize, usize)>>) -> u32 {
+    if cur == 9 {
+        match seen_ends {
+            Some(seen) => {
+                if !seen.contains(&(x, y)) {
+                    seen.insert((x, y));
+                    return 1;
+                }
+                return 0;
+            },
+            None => return 1
+        }
     }
     let mut sum = 0;
-    if x > 0 && map[y][x - 1] == cur + 1 { sum += find_trails(map, x - 1, y, cur + 1, seen_ends); }
-    if x < map[0].len() - 1 && map[y][x + 1] == cur + 1 { sum += find_trails(map, x + 1, y, cur + 1, seen_ends); }
-    if y > 0 && map[y - 1][x] == cur + 1 { sum += find_trails(map, x, y - 1, cur + 1, seen_ends); }
-    if y < map.len() - 1 && map[y + 1][x] == cur + 1 { sum += find_trails(map, x, y + 1, cur + 1, seen_ends); }
-    sum
-}
-
-fn find_ratings(map: &Vec<Vec<u32>>, x: usize, y: usize, cur: u32) -> u32 {
-    if cur == 9 { return 1; }
-    let mut sum = 0;
-    if x > 0 && map[y][x - 1] == cur + 1 { sum += find_ratings(map, x - 1, y, cur + 1); }
-    if x < map[0].len() - 1 && map[y][x + 1] == cur + 1 { sum += find_ratings(map, x + 1, y, cur + 1); }
-    if y > 0 && map[y - 1][x] == cur + 1 { sum += find_ratings(map, x, y - 1, cur + 1); }
-    if y < map.len() - 1 && map[y + 1][x] == cur + 1 { sum += find_ratings(map, x, y + 1, cur + 1); }
+    if x > 0 && map[y][x - 1] == cur + 1 { sum += find_trails_or_ratings(map, x - 1, y, cur + 1, seen_ends); }
+    if x < map[0].len() - 1 && map[y][x + 1] == cur + 1 { sum += find_trails_or_ratings(map, x + 1, y, cur + 1, seen_ends); }
+    if y > 0 && map[y - 1][x] == cur + 1 { sum += find_trails_or_ratings(map, x, y - 1, cur + 1, seen_ends); }
+    if y < map.len() - 1 && map[y + 1][x] == cur + 1 { sum += find_trails_or_ratings(map, x, y + 1, cur + 1, seen_ends); }
     sum
 }
 
@@ -30,32 +28,18 @@ fn main() {
     let args = args(BIN);
     let now = Instant::now();
 
+    let map: Vec<Vec<u32>> = args.input.lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+
     let solution: u32 = if !args.second {
-        let map: Vec<Vec<u32>> = args.input.lines()
-            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
-            .collect();
-        let mut sum = 0;
-        for y in 0..map.len() {
-            for x in 0..map[0].len() {
-                if map[y][x] == 0 {
-                    sum += find_trails(&map, x, y, 0, &mut HashSet::new());
-                }
-            }
-        }
-        sum
+        (0..map.len()).map(|y| (0..map[0].len()).map(move |x| (x, y))).flatten()
+            .filter_map(|(y, x)| if map[y][x] == 0 { Some(find_trails_or_ratings(&map, x, y, 0, &mut Some(HashSet::new()))) } else { None })
+            .sum()
     } else {
-        let map: Vec<Vec<u32>> = args.input.lines()
-            .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
-            .collect();
-        let mut sum = 0;
-        for y in 0..map.len() {
-            for x in 0..map[0].len() {
-                if map[y][x] == 0 {
-                    sum += find_ratings(&map, x, y, 0);
-                }
-            }
-        }
-        sum
+        (0..map.len()).map(|y| (0..map[0].len()).map(move |x| (x, y))).flatten()
+            .filter_map(|(y, x)| if map[y][x] == 0 { Some(find_trails_or_ratings(&map, x, y, 0, &mut None)) } else { None })
+            .sum()
     };
 
     result(solution, now.elapsed(), &args);
